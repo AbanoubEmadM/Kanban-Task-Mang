828d780ee993cac3d2c11df9c5b59827412293b6
beboEmad <=>
2023-07-05 23:49:20 +0300

    add new features

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { collection, doc, getDocs, getFirestore, setDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDocs, getFirestore, setDoc, updateDoc } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
@@ -20,61 +20,70 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-const marketingRef = collection(db, 'marketing-plan/todos/todo')
-const marketDiv = document.querySelector(".marketing-plan")
+const dataRef = collection(db, `${window.location.href.split('#')[1]}`)
 
-const roadmapRef = collection(db, 'roadmap/roads/road')
-const roadmapDiv = document.querySelector(".roadmap")
-
-const platformRef = collection(db, 'platform-launch/platforms/platform')
-const platformDiv = document.querySelector(".platform-launch")
-
-
-function docsAdd(ref, div) {
+function fetchDocs(ref) {
     getDocs(ref)
         .then((snapshots) => {
             let data = []
             snapshots.docs.forEach((doc) => {
                 data.push({...doc.data()})
             })
-            let title = ref._path.segments[0];
-            if(title === 'platform-launch'){
-                div.innerHTML = `
-                <div class='pre-plan'>
-                    <h4 class='status'>TODO</h4>
-                    <span style='background: #44C6E7'></span>
-                </div>`
-            } else if(title === 'marketing-plan'){
-                div.innerHTML = `
-                <div class='pre-plan'>
-                    <h4 class='status'>DONE</h4>
-                    <span style='background: #65DFAA'></span>
-                </div>
+            let div = document.querySelector(`.data-handler`)
+            let subDiv = `<div class='${ref._path.segments[1]}'></div>`
+            div.innerHTML += subDiv;
+            let div2 = document.querySelector(`.data-handler .${ref._path.segments[1]}`)
+            let title = ref._path.segments[2].split('-')[0];
+                    div2.innerHTML = `
+                    <div class='pre-plan'>
+                        <span class='${title}-span'></span>
+                        <h4>${ref._path.segments[2].split('-')[0]}</h4>
+                    </div>
                 `
-            } else {
-                div.innerHTML = `
-                <div class='pre-plan'>
-                    <h4 class='status'>DOING</h4>
-                    <span style='background: #645FC6'></span>
-                </div>
-                `
-            }
-            data.forEach((el) => {
-                div.innerHTML += `
-                <div class="plan">
-                    <h4> ${el.title}</h4> <br /> 
-                    <span> ${el.subtitle} </span> <br />
-                </div>`
+            data.forEach((item) => {
+                let itemDiv = `
+                    <div class='${ref._path.segments[2]}'>
+                        <h4>${item.title}</h4>
+                        <p>${item.subtitle}</p>
+                    </div>
+                        `
+                    div2.innerHTML += itemDiv
             })
     })
 }
-docsAdd(marketingRef, marketDiv)
-docsAdd(roadmapRef, roadmapDiv)
-docsAdd(platformRef, platformDiv)
 
+function showDocs(path) {
+    let ref1 = collection(db, `${path[0]}/todos/todo`)
+    let ref2 = collection(db, `${path[0]}/done/done-todos`)
+    let ref3 = collection(db, `${path[0]}/doing/doing-todos`)
+    fetchDocs(ref1)
+    fetchDocs(ref3)
+    fetchDocs(ref2)
+}
+showDocs(dataRef._path.segments)
+
+//change the link of db
+let links = document.querySelectorAll("aside ul li a")
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        setInterval(() => {
+            window.location.reload()
+        }, 300)
+    })
+})
+
+// add new task
 let addTask = document.querySelector(".add-task")
 let newTask = document.querySelector(".new-task")
 let overlay = document.querySelector(".overlay")
+let createTask = document.querySelector(".create-task")
+let options = document.querySelectorAll(".status option")
+let allSelects = document.querySelectorAll(".status select")
+let categoriesSelect = document.querySelector(".status select#categories").value
+let statusSelect = document.querySelector(".status select#state").value
+let title = document.querySelector(".title input")
+let subTitle = document.querySelector(".subtasks input")
 
 overlay.addEventListener("click", () => {
     newTask.classList.remove("active")
@@ -85,3 +94,4 @@ addTask.addEventListener("click", () => {
     newTask.classList.add("active")
     overlay.classList.add("active")
 })
+
